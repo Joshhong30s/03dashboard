7e09b3c290057bb5e517a36c83d5c9f0e63aa1ec
JJ-practice <fixymefixyou@gmail.com>
2023-04-23 17:57:05 +0800

    googlesheet

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,13 +1,38 @@
 /* eslint-disable react/jsx-key */
-import React, { useMemo } from 'react'
+import { useMemo } from 'react'
 import { useTable } from 'react-table'
-import MOCK_DATA from './MOCK_DATA.json'
-import { COLUMNS } from './columns'
 
-export default function BasicTable() {
-  // step3: create a table instance
-  const columns = useMemo(() => COLUMNS, [])
-  const data = useMemo(() => MOCK_DATA, [])
+export default function BasicTable({ data }) {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Day',
+        accessor: 'Day',
+      },
+      {
+        Header: 'Weight',
+        accessor: 'Weight',
+      },
+      {
+        Header: 'FeedingTime',
+        accessor: 'FeedingTime',
+      },
+      {
+        Header: 'FeedingVolume',
+        accessor: 'FeedingVolume',
+      },
+      {
+        Header: 'DiaperTime',
+        accessor: 'DiaperTime',
+      },
+      {
+        Header: 'DiaperColor',
+        accessor: 'DiaperColor',
+      },
+    ],
+    []
+  )
+
   const {
     getTableProps,
     getTableBodyProps,
@@ -16,48 +41,62 @@ export default function BasicTable() {
     rows,
     prepareRow,
   } = useTable({
-    columns: columns,
+    columns,
     data: data,
   })
 
   return (
-    //step4: define a table structure
-    <table className='w-full' {...getTableProps()}>
-      <thead className='bg-slate-500'>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                className='border-2 border-white'
-                {...column.getHeaderProps()}
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody className='text-center' {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+    <div className='overflow-x-auto'>
+      <table
+        className='w-full text-center border-collapse'
+        {...getTableProps()}
+      >
+        <thead className='bg-slate-600'>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  className='px-4 py-3 text-white font-semibold border-b-2 border-slate-400'
+                  {...column.getHeaderProps()}
+                >
+                  {column.render('Header')}
+                </th>
               ))}
             </tr>
-          )
-        })}
-      </tbody>
-      <tfoot className='text-center'>
-        {footerGroups.map((footerGroup) => (
-          <tr {...footerGroup.getFooterGroupProps}>
-            {footerGroup.headers.map((column) => (
-              <td {...column.getFooterProps}>{column.render('Footer')}</td>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()} className='even:bg-slate-100'>
+                {row.cells.map((cell) => (
+                  <td
+                    className='px-4 py-3 border-b border-slate-300'
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                ))}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td
+                  className='px-4 py-3 bg-slate-200 font-semibold border-t-2 border-slate-300'
+                  {...column.getFooterProps()}
+                >
+                  {column.render('Footer')}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
-import 'regenerator-runtime'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import ColumnOrder from '../components/ColumnOrder'
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <ColumnOrder />
-    </div>
-  )
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+export default MyApp
